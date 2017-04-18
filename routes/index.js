5c182090816295ea0d5003c26aeaa29530202927
aishwat <aishwat.singh@target.com>
2017-04-19 00:28:19 +0530

    till_questionnaire

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,33 @@ var router = express.Router();
 var users = require('./users.js');
 var auth = require('./auth.js');
 var email_transporter = require('./email_transporter.js');
-
+var questionnaire = require('./questionnaire.js');
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 
 // });
 // https://www.facebook.com/v2.8/dialog/oauth?%20client_id=1441995542511342&redirect_uri=http://localhost:3000/auth/facebook/callback&response_type=code&scope=email&auth_type=rerequest
+const authors = [
+{
+  id: 'cory-house',
+  firstName: 'Cory',
+  lastName: 'House'
+},
+{
+  id: 'scott-allen',
+  firstName: 'Scott',
+  lastName: 'Allen'
+},
+{
+  id: 'dan-wahlin',
+  firstName: 'Dan',
+  lastName: 'Wahlin'
+}
+];
 
+router.get('/',function(req,res){
+	res.send(authors);
+})
 
 router.post('/signup',auth.signup);
 router.get('/signup_verified',auth.signup_verified);
@@ -24,6 +44,12 @@ router.put('/user/save', users.save);
 router.get('/user/list', users.list);
 router.get('/user/:id', users.getById);
 router.delete('/user/:id', users.deleteById);
+
+router.get('/questionnaire/upload_form',questionnaire.upload_form)
+router.post('/questionnaire/upload',questionnaire.upload)
+router.get('/questionnaire/list',questionnaire.list)
+router.get('/questionnaire/getByTitle/:title',questionnaire.getByTitle)
+
 
 
 module.exports = router;

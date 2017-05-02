0bea716b6c67a3f597b42007fbd74cba21c3393d
Mayur Joshi <Mayur.Joshi@in.tesco.com>
2017-05-02 23:56:48 +0530

    Add in ec2 routes

--- src/scripts/utils/Constants.js
+++ src/scripts/utils/Constants.js
@@ -2,10 +2,10 @@ import ActionTypes from './ActionTypes';
 
 var Constants = {
     API: {
-        [ActionTypes.GET_QUESTION_CATEGORIES]: 'http://localhost:3000/categories',
-        [ActionTypes.GET_QUESTIONS]: 'http://localhost:3000/questions',
-        [ActionTypes.REGISTER_USER]: 'http://localhost:3000/signup',
-        [ActionTypes.LOGIN_USER]: 'http://localhost:3000/login'
+        [ActionTypes.GET_QUESTION_CATEGORIES]: 'http://ec2-35-154-248-81.ap-south-1.compute.amazonaws.com/categories',
+        [ActionTypes.GET_QUESTIONS]: 'http://ec2-35-154-248-81.ap-south-1.compute.amazonaws.com/questions',
+        [ActionTypes.REGISTER_USER]: 'http://ec2-35-154-248-81.ap-south-1.compute.amazonaws.com/signup',
+        [ActionTypes.LOGIN_USER]: 'http://ec2-35-154-248-81.ap-south-1.compute.amazonaws.com/login'
     }
 };
 

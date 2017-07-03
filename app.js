ca7d8d5ba037a0e56e3a4abcdc9b6a738c757e45
aishwat <aishwat.singh@target.com>
2017-07-03 23:13:10 +0530

    mapping_public_to_landing

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/landing',express.static(path.join(__dirname, 'public')));
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
